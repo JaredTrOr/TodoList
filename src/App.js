1369b2b1f33ab54783a8664e56571519cc4e2167
JaredOrTr <trujillojaredalexander@gmail.com>
2022-12-03 16:56:22 -0600

    Basic style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import './components/Button.css'
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <>
-      <div>Hello world</div>
+      <div className='container'>
+        <h2>Todo List</h2>
+        <input className='input' type="text"/>
+        <button className='btn btn-add'>Add todo</button>
+        <TodoList/>
+        <button className='btn btn-clear'>Clear todos</button>
+      </div>
     </>
   );
 }
