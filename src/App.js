fba59ed23ed5b43d381621c68b759f3bcb612f31
JaredOrTr <trujillojaredalexander@gmail.com>
2022-12-03 19:59:28 -0600

    There was added the funcionallity of clear todos

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import './components/Button.css'
 import TodoList from './components/TodoList';
 import Image from './components/Image';
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect} from 'react';
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'Todo-Storage';
   const [todos, setTodos] = useState([]);
   const inputTodo = useRef();
+
+  useEffect(() => {
+    const storagedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storagedTodos){
+      setTodos(storagedTodos);
+    }
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(){
     const name = inputTodo.current.value;
@@ -24,15 +36,27 @@ function App() {
     inputTodo.current.value = null;
   }
 
+  function clearTodos(){
+    const notCompletedTodos = todos.filter(todo => !todo.complete);
+    setTodos(notCompletedTodos);
+  }
+
+  function toggleTodo(id){
+    const copyTodos = [...todos];
+    const todo = copyTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(copyTodos);
+  }
+
   return (
     <>
       <div className='container'>
         <h1>Todo List</h1>
         <input ref={inputTodo} className='input' type="text"/>
         <button className='btn btn-add' onClick={addTodo}>Add todo</button>
-        <TodoList todos={todos} className={todos ? 'visible' : 'noVisible'}/>
+        <TodoList todos={todos} className={todos ? 'visible' : 'noVisible'} toggleTodo={toggleTodo}/>
         {todos.length === 0 && <Image/>}
-        <button className='btn btn-clear'>Clear todos</button>
+        <button className='btn btn-clear' onClick={clearTodos}>Clear todos</button>
       </div>
     </>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,15 @@
 import './Todo.css';
 
-export default function Todo({todo}){
+export default function Todo({todo, toggleTodo}){
+    
+    function todoClick(){
+        toggleTodo(todo.id);
+    }
+
     return(
         <>  
             <div className="todo">
-                <input className='check' type='checkbox'/>
+                <input className='check' type='checkbox' onChange={todoClick}/>
                 {todo.name}
             </div> 
         </>
